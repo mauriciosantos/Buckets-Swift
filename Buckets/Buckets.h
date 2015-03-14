65e7ca72f97db9554719392b7db8949f5da8826c
Mauricio Santos <mauricio.santos@globant.com>
2015-03-14 15:39:38 -0500

    Initial Commit

---
+++ Buckets/Buckets.h
@@ -0,0 +1,19 @@
+//
+//  Buckets.h
+//  Buckets
+//
+//  Created by Mauricio Santos on 3/14/15.
+//  Copyright (c) 2015 Mauricio Santos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Buckets.
+FOUNDATION_EXPORT double BucketsVersionNumber;
+
+//! Project version string for Buckets.
+FOUNDATION_EXPORT const unsigned char BucketsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Buckets/PublicHeader.h>
+
+
